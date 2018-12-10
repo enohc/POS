985003e5368ad184ca9d62e27dd3f38547efb15e
enohc <enohc.flores@yahoo.es>
2018-12-09 22:21:06 -0600

    Mto para roles y usuarios

---
+++ POS/Controllers/RolController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using POS.Model;
+
+namespace POS.Controllers
+{
+    public class RolController : Controller
+    {
+        private POSDataContext db = new POSDataContext();
+
+        // GET: Menu
+        public ActionResult Index()
+        {
+            List<ROL> rol = db.ROLs.ToList();
+            return View(rol);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(ROL m)
+        {
+            db.ROLs.InsertOnSubmit(m);
+            db.SubmitChanges();
+            return Redirect("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int? id)
+        {
+            ROL u = db.ROLs.Where(m => m.idRol ==id).Single();
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ROL m)
+        {
+            db.ROLs.GetModifiedMembers(m);
+            db.SubmitChanges();
+            return Redirect("Index");
+        }
+    }
+
+}
---
+++ POS/Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using POS.Model;
+namespace POS.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private POSDataContext db = new POSDataContext();
+
+        // GET: Menu
+        public ActionResult Index()
+        {
+            List<USUARIO> menus = db.USUARIOs.ToList();
+            return View(menus);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(USUARIO m)
+        {
+            db.USUARIOs.InsertOnSubmit(m);
+            db.SubmitChanges();
+            return Redirect("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Editar(string id)
+        {
+            USUARIO u = db.USUARIOs.Where(m => m.DUI.Equals(id)).Single();
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(USUARIO m)
+        {
+            db.USUARIOs.GetModifiedMembers(m);
+            db.SubmitChanges();
+            return Redirect("Index");
+        }
+    }
+}
